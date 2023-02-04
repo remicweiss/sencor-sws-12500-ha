37995079ab1e3accd490c879591d19fa871578ef
Ziga Brinsek <ziga.brinsek@gmail.com>
2023-02-04 20:22:52 +0100

    Create sencor-sws-12500-ha.py
    First script :)

---
+++ sencor-sws-12500-ha.py
@@ -0,0 +1,85 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import paho.mqtt.client as mqtt
+import time
+import sys
+
+#test command wget -qO- "http://localhost:8080/weatherstation/updateweatherstation.php?ID=55&PASSWORD=asdfghjkl&action=updateraww&realtime=1&rtfreq=5&dateutc=now&baromin=29.91&tempf=74.3&dewptf=41.9&humidity=31&windspeedmph=1.7&windgustmph=1.7&winddir=0&rainin=0.0&dailyrainin=0.0&solarradiation=0.23&UV=0.0&indoortempf=76.8&indoorhumidity=26&soiltempf=73.2&soilmoisture=35" &> /dev/null
+
+#test run python3 vreme.py <ip> 1883 <mqtt_user> <mqtt_pass> sencor_sws_12500 8080
+
+mqtt_ip = sys.argv[1]
+mqtt_port = sys.argv[2]
+mqtt_user = sys.argv[3]
+mqtt_pass = sys.argv[4]
+sensor_name_prefix = sys.argv[5]
+http_port = sys.argv[6]
+
+
+client = mqtt.Client()
+client.username_pw_set(mqtt_user, mqtt_pass)
+client.connect(mqtt_ip, int(mqtt_port), 60)
+client.loop_start()
+
+init = True
+
+
+sensors_config ={
+'baromin' : '"device_class" : "atmospheric_pressure"',
+'tempf' : '"device_class" : "temperature"',
+'dewptf': '"device_class" : "temperature"',
+'humidity': '"device_class" : "humidity"',
+'windspeedmph' : '"device_class" : "wind_speed"',
+'windgustmph' : '"device_class" : "wind_speed"',
+'winddir' : '"device_class" : "None"',
+'rainin' : '"device_class" : "distance"',
+'dailyrainin' : '"device_class" : "distance"',
+'solarradiation' : '"device_class" : "irradiance"',
+'UV' : ' "device_class" : "None"',
+'indoortempf' : '"device_class" : "temperature"',
+'indoorhumidity' : '"device_class" : "humidity"',
+'soiltempf' : '"device_class" : "temperature"',
+'soilmoisture' : ' "device_class" : "humidity"'
+}
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+	def get_config(self, sensor):
+		config_data = '{{"name" : "{0}_{1}", "state_topic" : "homeassistant/sensor/{0}/state", "value_template": "{{{{ value_json.{1}}}}}", {2}}}'.format(sensor_name_prefix, sensor, sensors_config[sensor])
+		return config_data
+
+	def do_GET(self):
+		global init
+		send_data = ""
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		url = self.path.split('?')
+
+		if url[0] == "/weatherstation/updateweatherstation.php":
+			data = url[1].split('&')
+		
+			for x in data:
+				sensor = x.split('=')
+				if sensor[0] in sensors_config:
+					if init:
+						client.publish("homeassistant/sensor/{}/config".format(sensor_name_prefix + sensor[0]), self.get_config(sensor[0]))
+					send_data += '"{}":{},'.format(sensor[0],sensor[1])
+			client.publish("homeassistant/sensor/{}/state".format(sensor_name_prefix), "{" + send_data[:-1] + "}")
+		
+		init = False
+
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer(('', int(http_port)), MyServer)
+    print("Server started http://%s:%s" % ('*', http_port))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+    client.loop_stop()
